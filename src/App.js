adcd99773478a709eb22d0728b228536e102b4b8
Amy Lam <amy@Marcos-MBP.hsd1.ca.comcast.net>
2021-09-09 14:22:05 -0700

    First Submission

--- src/App.js
+++ src/App.js
@@ -7,26 +7,44 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
 	};
+
+	const removeItem = item => {
+		setCart(cart.filter(product => (item.id !== product.id)));
+	}
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<CartContext.Provider value={cart}>
+				<Navigation />
+			</CartContext.Provider>
+
 
 			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+			<ProductContext.Provider value={{ products, addItem }}>
+				<Route exact path="/">
+					<Products />
+				</Route>
+			</ProductContext.Provider>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+			<CartContext.Provider value={{cart, removeItem}}>
+			{/* <CartContext.Provider value={cart}> */}
+				<Route path="/cart">
+					<ShoppingCart />
+				</Route>
+			</CartContext.Provider>
+
+
 		</div>
 	);
 }
